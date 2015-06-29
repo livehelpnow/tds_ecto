5a398782f427779531d7e5965a9927db8dafdf3a
Justin Schneck <jschneck@mac.com>
2015-06-29 09:29:53 -0400

    Bump versions for ecto 0.13

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule TDS.Ecto.Mixfile do
 
   defp deps do
     [
-      {:ecto, "~> 0.12.1"},
-      {:tds, "~> 0.2"},
+      {:ecto, "~> 0.13.0"},
+      {:tds, "~> 0.3"},
       {:poison, only: :test}
     ]
   end
