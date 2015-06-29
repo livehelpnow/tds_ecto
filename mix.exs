50f574a67448c7b200b2d20e7f0c12478d9d0ade
Justin Schneck <jschneck@mac.com>
2015-06-29 10:09:05 -0400

    Release 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TDS.Ecto.Mixfile do
   def project do
     [
       app: :tds_ecto,
-      version: "0.2.6-dev",
+      version: "0.3.0",
       elixir: "~> 1.0",
       deps: deps,
       description: description,
