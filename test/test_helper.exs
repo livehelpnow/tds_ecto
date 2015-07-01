9b19c28e6683591480f70d98ecb220076706897f
Justin Schneck <jschneck@mac.com>
2015-07-01 09:06:04 -0400

    require ecto pool tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -81,6 +81,7 @@ Code.require_file "../deps/ecto/integration_test/sql/migration.exs", __DIR__
 Code.require_file "../deps/ecto/integration_test/sql/escape.exs", __DIR__
 Code.require_file "../deps/ecto/integration_test/cases/repo.exs", __DIR__
 Code.require_file "../deps/ecto/integration_test/cases/type.exs", __DIR__
+Code.require_file "../deps/ecto/integration_test/cases/pool.exs", __DIR__
 Code.require_file "../deps/ecto/integration_test/cases/preload.exs", __DIR__
 
 
