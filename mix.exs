b249770482a07307006ee5bc4d9211eac5dcfce7
Daniel Icardo <daniel.icardo@poincenot.com>
2015-12-14 13:45:50 -0300

    Prepare for release.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TDS.Ecto.Mixfile do
   def project do
     [
       app: :tds_ecto,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.0",
       deps: deps,
       description: description,
