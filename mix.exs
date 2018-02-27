bad6a4fc3cd6dd0fc23367913be466baf17322d3
Milan Jaric <milan.jaric@gmail.com>
2018-02-27 17:46:40 +0100

    fixing tds dependency

--- mix.exs
+++ mix.exs
@@ -24,8 +24,7 @@ defmodule TDS.Ecto.Mixfile do
   defp deps() do
     [
       {:ecto, ">= 2.0.0"},
-      # {:tds, "~> 1.0"},
-      {:tds, path: "../tds"},
+      {:tds, ">= 1.0.14"},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:poison, ">= 0.0.0", only: :test}
     ]
